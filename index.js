0e50259f06553358f6d857fb8c02aa405fbb0d42
Julie17XI <xi.tang717@gmail.com>
2022-07-18 22:55:07 -0500

    changed quotation marks

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({hi: 'peach green tea'});
+    res.send({hi: "peach green tea"});
 });
 
 const PORT = process.env.PORT || 1000
