6ea9d7c9236d116b8e2bd418fbbcfa4c792f933e
Julie17XI <xi.tang717@gmail.com>
2022-07-23 10:03:46 -0500

    changed configurations

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@
 module.exports = {
     googleClientID: '275786397144-bose88d31fd42ivcdldereorr0src5q4.apps.googleusercontent.com',
     googleClientSecret: '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',
-    mongoURI: 'mongodb+srv://Julie17_XI:Fall2022@cluster0.g0jxege.mongodb.net/ideapop?retryWrites=true&w=majority',
+    mongoURI: 'mongodb+srv://Julie17_XI:Fall2022SA@cluster0.g0jxege.mongodb.net/ideapop?retryWrites=true&w=majority',
     cookieKey: 'corgisamoyedicecappedmountains'
 };
